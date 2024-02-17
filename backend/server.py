f87a45c2fb09aa892a30338771e05c8b341dee8f
sipneat <jake.esperson@outlook.com>
2024-02-17 15:02:39 -0800

    setup

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/NorthEast')
+
+@app.route("/NorthEast")
 def NorthEast():
     return {"NorthEast": "Hello from NorthEast!"}
 
-if __name__ == '__main__':
+
+@app.route("/getPop")
+def getPop():
+    with open("gb.json") as f:
+        data = json.load(f)
+    return data
+
+
+if __name__ == "__main__":
     app.run(debug=True)
