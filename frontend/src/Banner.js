3a7012eea2bf3e97f67eb9a292ae444634e81c8a
austinnguyen26 <austinnguyen426@gmail.com>
2024-02-17 11:28:26 -0800

    Update Banner.js

--- frontend/src/Banner.js
+++ frontend/src/Banner.js
@@ -1,10 +1,21 @@
-function Banner(){
+import React from 'react';
 
-
+function Banner({ message, backgroundColor, color }) {
+    const bannerStyle = {
+      backgroundColor: backgroundColor || '#007bff', // Blue right now, but change later according to theme
+      color: color || '#ffffff', // change color to intended scheme
+      padding: '10px',
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: '18px',
+      marginBottom: '20px',
+    };
+  
     return (
-        <div className="bold">hello world</div>
-       
+      <div style={bannerStyle}>
+        {message}
+      </div>
     );
-}
-
-export default Banner;
+  }
+  
+  export default Banner;
