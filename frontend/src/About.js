33cdfdc8578820d8e5083957ca6654508a97aaac
austinnguyen26 <austinnguyen426@gmail.com>
2024-02-17 23:02:24 -0800

    Update About.js

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function About() {
-    
+    const navigate = useNavigate();
+
     return (
+        
         <div className=" flex flex-col justify-center items-center">
-            <h1 className=" font-bold">About VirusVision</h1>
+            <h1 className="mt-11 text-xl font-bold">About VirusVision</h1>
             <p className="text-center p-2">
                 VirusVision is a tool for visualizing and analyzing data from the
                 Allen Brain Observatory. It was developed by the Allen Institute
@@ -10,18 +14,24 @@ function About() {
                 functional imaging datasets that are being collected by the
                 Institute.
             </p>
-            <h2 className=" font-bold">How to use VirusVision</h2>
+            <h2 className="mt-11 text-xl font-bold">How to use VirusVision</h2>
             <p className="text-center p-3">
             VirusVision is an interactive 3-Dimensional Map that uses Deck-Gl and displays a map of the UK and 
                 displays statistics for COVID-19 and the Flu. As you view different areas,  
             </p>
-            <h3 className=" font-bold">About Our Mission</h3>
+            <h3 className="mt-11 text-xl font-bold">About Our Mission</h3>
             <p className="text-center p-4">
                 Our Team, composed of 6 members, Grant, Johnathan, Jake Esperson, Anish Katragadda, Ian Kennar, and Austin Nguyen, 
                 set out on the mission to improve the public's ability to perceive statistcis regarding vital diseases. After several
                 team meetings, we decided on this interface due to it being aesthetically pleasing to the eye, as well as very easy to 
                 digest from an unfamiliar perspective on the subject
             </p>
+            <button
+                className=" border-2 border-black p-2 m-2 rounded-lg bg-sky-500 hover:bg-sky-700 text-sky-100 font-bold py-2 px-4"
+                onClick={() => navigate("/")}
+            >
+                Home Page
+            </button>
         </div>
     );
 }
