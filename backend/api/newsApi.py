98e9b21023e79f539cc1f92b4cd572f966f9d921
Anish K <47697221+AnishKatr@users.noreply.github.com>
2024-02-18 01:17:47 -0800

    news data also includes flu

--- backend/api/newsApi.py
+++ backend/api/newsApi.py
@@ -5,6 +5,10 @@ from newsapi import NewsApiClient
 newsApi = NewsApiClient(api_key='d40825b3538f413ea7b096c651d1c8e6')
 
 top_headlines = newsApi.get_everything(q='covid', sources='bbc-news', language='en')
+flu_headlines = newsApi.get_everything(q='flu', sources='bbc-news', language='en')
+
+flu_output = json.dumps(flu_headlines, indent=4)
+print(flu_output)
 
 json_ouput = json.dumps(top_headlines, indent=4)
 print(json_ouput)
@@ -12,4 +16,5 @@ print(json_ouput)
 output_file_path = "newsDump.json"
 with open(output_file_path, "w") as output_file:
      output_file.write(json_ouput)
+     output_file.write(flu_output)
 
