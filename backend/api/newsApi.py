51425837ce45f58403cb3b9e7452698af07775c5
sipneat <jake.esperson@outlook.com>
2024-02-22 20:20:20 -0800

    cleanup post-hackathon

--- backend/api/newsApi.py
+++ backend/api/newsApi.py
@@ -1,11 +1,16 @@
 import json
+from dotenv import load_dotenv
+import os
 
 from newsapi import NewsApiClient
 
-newsApi = NewsApiClient(api_key='d40825b3538f413ea7b096c651d1c8e6')
+load_dotenv()
 
-top_headlines = newsApi.get_everything(q='covid', sources='bbc-news', language='en')
-flu_headlines = newsApi.get_everything(q='flu', sources='bbc-news', language='en')
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+newsApi = NewsApiClient(api_key=NEWS_API_KEY)
+
+top_headlines = newsApi.get_everything(q="covid", sources="bbc-news", language="en")
+flu_headlines = newsApi.get_everything(q="flu", sources="bbc-news", language="en")
 
 flu_output = json.dumps(flu_headlines, indent=4)
 print(flu_output)
@@ -15,6 +20,5 @@ print(json_ouput)
 
 output_file_path = "newsDump.json"
 with open(output_file_path, "w") as output_file:
-     output_file.write(json_ouput)
-     output_file.write(flu_output)
-
+    output_file.write(json_ouput)
+    output_file.write(flu_output)
